a4ebb81c8f5acb6ea63cb17f19fc716e0ec21d05
AugustinChamoussetESEO <augustin.chamousset@reseau.eseo.fr>
2025-01-15 11:17:54 +0100

    Methodes ECUE
    - ajouteEnseignant
    - supprimerEnseignant

--- ecue.h
+++ ecue.h
@@ -12,11 +12,13 @@ class ECUE
 private:
     std::string nomECUE;
     int nbHeures;
+    std::vector<Enseignant> enseignants;
 
 public:
     ECUE(std::string nom, float nbHeures);
-    void ECUE::ajouterEnseignant(Enseignant enseignant);
-    void ECUE::supprimerEnseignant(Enseignant enseignant);
+
+    void ECUE::ajouterEnseignant(Enseignant e);
+    void ECUE::supprimerEnseignant(Enseignant e);   
 };
 
 #endif // ECUE_H
